c7c3a84181c837b529d103e4132230f33bb76d38
ari123rm <arielmoraes05@gmail.com>
2024-09-30 22:30:50 -0300

    Feat: add imagem capa

--- src/components/home/Capa.tsx
+++ src/components/home/Capa.tsx
@@ -5,9 +5,9 @@ function Capa(){
         <div className={css.capa}>
             <div>
                 <h1>Tabela Dinâmica</h1>
-                <p>Visualize seus projetos de forma dinâmica</p>
+                <h2>Visualize seus projetos de forma dinâmica</h2>
             </div>
-            
+                <img src="https://cdn-icons-png.flaticon.com/512/4675/4675642.png" alt="Imagem de uma tabela" />
         </div>
     )
 }
