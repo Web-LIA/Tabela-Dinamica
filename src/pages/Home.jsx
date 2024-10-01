aa32d0f171820bfcfd879faf8c8eaa0c65ab6c94
ari123rm <arielmoraes05@gmail.com>
2024-09-30 21:05:26 -0300

    Fix: modificado index.css para padrao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-
+import css from '../components/themes/home.module.css'
 
 function Home(){
     return (
-            <div>
+            <div className={css.Home}>
                 <h1>Tabela Dinâmica</h1>
                 <p>Utilize nossos serviços de tabela para seus projetos</p>
             </div>
