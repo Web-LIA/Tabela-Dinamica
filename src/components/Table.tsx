745ee3aea4f7bca35252c23aba7bc3669c8a4121
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2024-10-04 12:53:32 -0300

    feat/test: adicionar feat de inserir nova linha na tabela

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './Table.css';
 import TableRow from './TableRow';
 
@@ -9,15 +10,40 @@ type Row = {
 } // ainda é teste. futuramente por os tipos em outro lugar
 
 function Table() {
-    const data:Row[] = [
-        {id: 1, nome: 'Ryan', idade: 20},
+    const [data, setData]  = useState<Row[]>([
+        {id: 1, nome:'Ryan', idade:20},
         {id: 2, nome: 'Ariel', idade: 19},
         {id: 3, nome: 'Dede', idade: 19}
-    ];
 
-    const keys = Object.keys(data[0]);
+    ])
+    const [newData, setNewData] = useState<Row>({id: 0, nome: '', idade: 0})
+    const keys: string[] = Object.keys(data[0]);
 
+    function updateNewData(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.id === 'id'){
+            setNewData({...newData, id: parseInt(e.target.value)})
+        }
+        if (e.target.id === 'nome'){
+            setNewData({...newData, nome: e.target.value})
+        }
+        if (e.target.id === 'idade'){
+            setNewData({...newData, idade: parseInt(e.target.value)})
+        }
+    }
+
+    function addRow(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        setData([...data, {id: newData.id, nome: newData.nome, idade:newData.idade}])
+    }
+    
     return (
+        <>
+        <form onSubmit={addRow}>
+            <input id='id' type='number' placeholder='id' onChange={updateNewData}/>
+            <input id='nome' type='text' placeholder='nome' onChange={updateNewData}/>
+            <input id='idade' type='number' placeholder='idade' onChange={updateNewData}/>
+            <button type='submit'>Adicionar</button>
+        </form>
         <table>
             <thead>
                 <tr>
@@ -34,6 +60,7 @@ function Table() {
                 }
             </tbody>
         </table>
+        </>
     )    
 }
 
