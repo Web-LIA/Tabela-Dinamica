1db5f09730ceff894c0a1c53b3cb4ff75015e4af
janainafmonteiro <janainaferreiramonteiro@hotmail.com>
2024-10-10 10:06:07 -0300

    feat: criar butao editar

--- src/components/newTable/TableRow.tsx
+++ src/components/newTable/TableRow.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { RowData, TableRowMethods, TableRowProps } from "../../typesNewTable"
 import themes from '../themes/Table.module.css'
-
+import editarImg from '../../assets/home/Edit.png'
 function TableRow(props: TableRowProps) {
     const rowData = props.rowData
     return (
@@ -12,7 +12,7 @@ function TableRow(props: TableRowProps) {
             { props.editMode && (
                 <>
                 <td>
-                    <button onClick={() => props.methods.setEditRowId(rowData.id)}>Editar</button>
+                    <button onClick={() => props.methods.setEditRowId(rowData.id)} className={themes.editar}> <img src={editarImg} alt="Editar" /> </button>
                 </td>
                 <td>
                     <button onClick={() => props.methods.removeRow(rowData.id)} className={themes.remover}>X</button>
